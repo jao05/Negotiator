f66bf2c594c1809061e44fbc7f79b39cae44b9cd
Jesse Outlaw <JesseOutlaw03@gmail.com>
2018-09-12 12:51:07 -0400

    Added Done button to Negotiator Signup Page

--- public/index.js
+++ public/index.js
@@ -132,6 +132,9 @@ function makeUserTypeSelection()
 
         // Hide the Start Page
         $('.startPage').hide();
+
+        // Show Negotiator Sign-up page
+        $('.negotiatorSignupPage').show();
     });       
 }
 
