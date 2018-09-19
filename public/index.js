c375a0225f03f540ee7faf5dec27ec3d1315715c
Jesse Outlaw <JesseOutlaw03@gmail.com>
2018-09-19 14:31:15 -0400

    Got rid of all refs to mock data

--- public/index.js
+++ public/index.js
@@ -1,56 +1,3 @@
-let MOCK_NEGOTIATOR_AGENTS = {
-    "agents": [
-        {
-            "id": "1111111",
-            "expertise": "Cars",            
-            "agentName": "John Carson",
-            "metroArea": "New York City"           
-        },
-        {
-            "id": "2222222",
-            "expertise": "Cars",            
-            "agentName": "Jane Doe",
-            "metroArea": "Atlanta"          
-        },
-        {
-            "id": "333333",
-            "expertise": "Homes",            
-            "agentName": "Jim Homer",
-            "metroArea": "Los Angeles"         
-        },
-        {
-            "id": "4444444",
-            "expertise": "Boats",            
-            "agentName": "Jackie Boatman",
-            "metroArea": "Miami"          
-        },
-        {
-            "id": "555555",
-            "expertise": "Cars",            
-            "agentName": "Joe Schmoe",
-            "metroArea": "New York City"           
-        },
-        {
-            "id": "666666",
-            "expertise": "Planes",            
-            "agentName": "Jet Li",
-            "metroArea": "Hong Kong"          
-        },
-        {
-            "id": "777777",
-            "expertise": "Homes",            
-            "agentName": "Napoleon Bonaparte",
-            "metroArea": "Paris"         
-        },
-        {
-            "id": "888888",
-            "expertise": "Homes",            
-            "agentName": "Hillary Bush",
-            "metroArea": "Miami"          
-        }
-    ]
-};
-
 let cityChoice;
 let userFirstName;
 let userLastName;
@@ -378,37 +325,8 @@ function chooseDifferentItem()
     selectItemAndAddDetail();
 }
 
-
-// ************** TAKEN FORM THINKFUL PROGRAM - TOP ****************************
-
-function getAgents(callbackFn) {
-    setTimeout(function(){ callbackFn(MOCK_NEGOTIATOR_AGENTS)}, 100);
-}
-
-// this function stays the same when we connect
-// to real API later
-function displayAgents(data) {
-    for (index in data.agents) {
-       $('#matchedAgents').html(
-        '<p>' + data.agents[index].agentName + '</p>');
-    }
-}
-
-// this function can stay the same even when we
-// are connecting to real API
-function getAndDisplayAgents() {
-    getAgents(displayAgents);
-
-    // Hide Item Detail Page
-    $('.itemDetailPage').hide();
-
-    // Show Agents List
-    $('.chooseNegotiatorPage').show();
-}
-
 $(function() {
     
     renderLandingPage();    
 })
 
-// ************** TAKEN FORM THINKFUL PROGRAM - BOTTOM ****************************
